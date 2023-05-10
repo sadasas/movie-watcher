5822b9f55267a5e8c77743144f6f7cd503871590
wahyusrp <setio101199@gmail.com>
2023-05-10 13:00:03 +0700

    Redux Persist does not allow correct SSG causing terrible SEO https://github.com/vercel/next.js/issues/20926

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,8 +5,7 @@ import "@/styles/globals.scss";
 import Navbar from "@/components/navbar/Navbar";
 import Footer from "@/components/Footer";
 import { Provider } from "react-redux";
-import { persistor, store } from "@/store";
-import { PersistGate } from "redux-persist/integration/react";
+import { store } from "@/store";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 const queryClient = new QueryClient();
@@ -15,15 +14,13 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <Head>
-            <title>JustSee | Enjoy your movie</title>
-            <meta name="description" content="enjoy your movie" key="desc" />
-          </Head>
-          <Navbar />
-          <Component {...pageProps} />
-          <Footer />
-        </PersistGate>
+        <Head>
+          <title>JustSee | Enjoy your movie</title>
+          <meta name="description" content="enjoy your movie" key="desc" />
+        </Head>
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer />
       </Provider>
     </QueryClientProvider>
   );
--- src/store/index.ts
+++ src/store/index.ts
@@ -3,27 +3,10 @@ import {
   configureStore,
   getDefaultMiddleware,
 } from "@reduxjs/toolkit";
-import storage from "redux-persist/lib/storage";
-
-import {
-  persistReducer,
-  persistStore,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
 
 import bookmarkReducer from "@/features/bookmark/bookmarkSlice";
 import bookmarkNotificationReducer from "@/features/bookmark/bookmarkNotificationSlice";
 import popupTrailerReducer from "@/features/popup/popupTrailerSlice";
-
-const persistConfig = {
-  key: "root",
-  storage,
-};
 
 const rootReducer = combineReducers({
   bookmark: bookmarkReducer,
@@ -31,19 +14,11 @@ const rootReducer = combineReducers({
   popupTrailer: popupTrailerReducer,
 });
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 export const store = configureStore({
   reducer: {
-    reducer: persistedReducer,
+    reducer: rootReducer,
   },
-  middleware: getDefaultMiddleware({
-    serializableCheck: {
-      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-    },
-  }),
 });
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
-export const persistor = persistStore(store);
