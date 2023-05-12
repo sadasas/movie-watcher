51dee7de6ecece3f3c2ba798a172367b9edf3789
wahyusrp <setio101199@gmail.com>
2023-05-12 08:52:32 +0700

    fix

--- src/pages/api/movieApi.ts
+++ src/pages/api/movieApi.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const movieApi = axios.create({
   baseURL: "https://moviesdatabase.p.rapidapi.com",
   headers: {
-    "X-RapidAPI-Key": "bcf62e5e45msh2eee34009386b14p199dc3jsn52392703a308",
-    "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
+    "X-RapidAPI-Key": NEXT_PUBLIC_X_RAPIDAPI_KEY,
+    "X-RapidAPI-Host": NEXT_PUBLIC_X_RAPIDAPI_HOST,
   },
 });
